d7393ac3d97eeb2c7477ce73e1aa90283033d643
thebrightlee1 <thebrightlee1@gmail.com>
2022-06-20 03:05:33 +0900

    slider auto play

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.js-main-visual').slick({
-        // autoplay: true,
+        autoplay: true,
         prevArrow: document.querySelector('.js-arrow-prev'),
         nextArrow: document.querySelector('.js-arrow-next')
     });
